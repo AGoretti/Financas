96399420676b31ef187ffe31091cea763bb52469
Agoretti <andremotta96@gmail.com>
2020-07-03 15:48:50 -0300

    dataframe concat

--- valores.py
+++ valores.py
@@ -3,13 +3,14 @@ from selenium import webdriver
 from time import sleep
 import requests
 import csv
+import pandas as pd
 
 
 ticks = ['AALR3',
          'AFLT3',
          'AGRO3',
          'ALSO3',
-         'ALUP1',
+         'ALUP11',
          'ALUP3',
          'ALUP4',
          'AMAR3',
@@ -25,7 +26,7 @@ ticks = ['AALR3',
          'BDLL3',
          'BDLL4',
          'BEEF3',
-         'BIDI1',
+         'BIDI11',
          'BIDI3',
          'BIDI4',
          'BIOM3',
@@ -92,7 +93,6 @@ ticks = ['AALR3',
          'CVCB3',
          'CYRE3',
          'DIRR3',
-         'DMMO1',
          'DMMO3',
          'EALT3',
          'EALT4',
@@ -115,7 +115,6 @@ ticks = ['AALR3',
          'GEPA4',
          'GFSA3',
          'GOAU4',
-         'GOLL1',
          'GOLL4',
          'GRND3',
          'GSHP3',
@@ -132,7 +131,7 @@ ticks = ['AALR3',
          'ITUB4',
          'JBSS3',
          'JHSF3',
-         'KLBN1',
+         'KLBN11',
          'KLBN3',
          'KLBN4',
          'LAME3',
@@ -150,7 +149,7 @@ ticks = ['AALR3',
          'MDIA3',
          'MDNE3',
          'MGLU3',
-         'MMXM1',
+         'MMXM11',
          'MMXM3',
          'MNDL3',
          'MOVI3',
@@ -172,6 +171,77 @@ ticks = ['AALR3',
          'PETR4',
          'PMAM3',
          'PNVL3',
+         'PNVL4',
+         'POMO3',
+         'POMO4',
+         'POSI3',
+         'PRIO3',
+         'PSSA3',
+         'QUAL3',
+         'RADL3',
+         'RAIL3',
+         'RANI3',
+         'RANI4',
+         'RAPT3',
+         'RAPT4',
+         'RDNI3',
+         'RENT3',
+         'RLOG3',
+         'RNEW11',
+         'RNEW3',
+         'RNEW4',
+         'RPMG3',
+         'RSID3',
+         'SANB11',
+         'SANB3',
+         'SANB4',
+         'SAPR11',
+         'SAPR3',
+         'SAPR4',
+         'SBSP3',
+         'SCAR3',
+         'SEER3',
+         'SHUL3',
+         'SHUL4',
+         'SLED3',
+         'SLED4',
+         'SMLS3',
+         'SMTO3',
+         'STBP3',
+         'SUZB3',
+         'TAEE11',
+         'TAEE3',
+         'TAEE4',
+         'TASA3',
+         'TASA4',
+         'TCSA3',
+         'TELB3',
+         'TELB4',
+         'TIET11',
+         'TIET3',
+         'TIET4',
+         'TIMP3',
+         'TOTS3',
+         'TPIS3',
+         'TRPL3',
+         'TRPL4',
+         'UGPA3',
+         'UNIP3',
+         'UNIP5',
+         'UNIP6',
+         'USIM3',
+         'USIM5',
+         'USIM6',
+         'VALE3',
+         'VIVA3',
+         'VIVT3',
+         'VIVT4',
+         'VLID3',
+         'VVAR3',
+         'WEGE3',
+         'WHRL3',
+         'WHRL4',
+         'YDUQ3'
          ]
 
 ticks2 = ['AALR3']
@@ -187,17 +257,25 @@ header = {
     "X-Requested-With": "XMLHttpRequest"
 }
 
-cabeca = 'Tick', 'Cotacao', 'N.acoes', 'Ebit'
+cabeca = 'Tick', 'Cotacao', 'Setor', 'Vol. Med', 'Valor Mercado', 'Valor Firma', 'N.acoes', 'P/L', 'LPA', 'P/VP', 'VPA',  'P/EBIT', 'Marg. Bruta',  'PSR', 'Marg. EBIT', 'Marg. Líquida', 'ROIC', 'Div. Yield', 'ROE',  'EV / EBITDA', 'EV / EBIT', 'Cres. Rec (5a)', 'Ativo', 'Dív. Bruta', 'Disponibilidades', 'Dív. Líquida', 'Ativo Circulante', 'Patrim. Líq', 'Receita Líquida 12', 'Receita Líquida 3',  'Ebit 12', 'Ebit 3', 'Lucro Líquido 12', 'Lucro Líquido 3'
+
+cabecaF = 'Tick', 'Cotacao', 'Setor', 'Vol. Med', 'Valor Mercado', 'Valor Firma', 'N.acoes', 'P/L', 'LPA', 'P/VP', 'VPA',  'P/EBIT', 'Marg. Bruta',  'PSR', 'Marg. EBIT', 'Marg. Líquida', 'ROIC', 'Div. Yield', 'ROE',  'EV / EBITDA', 'EV / EBIT', 'Cres. Rec (5a)', 'Ativo', 'Patrim. Líq', 'Lucro Líquido 12', 'Lucro Líquido 3'
 
 
-with open('valores.csv', 'a', newline='') as file:
+with open('valores.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(cabeca)
 
 
+with open('valoresF.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(cabecaF)
+
 for tick in ticks:
 
-    cont = 0
+    contebit = 0
+
+    contativo = 0
 
     ticksValores = []
 
@@ -220,22 +298,105 @@ for tick in ticks:
 
     for td in tds:
 
-        if cont == 6:
+        if contebit == 6:
             select = True
-            cont = cont + 1
+            contebit = contebit + 1
+
+        if contebit == 8:
+            select = True
+            contebit = contebit + 1
+
+        if contativo == 4:
+            select = True
+            contativo = contativo + 1
 
         if select == True:
             select = False
-            ticksValores.append(td.text)
+            temp = td.text
+            if temp.startswith('\n'):
+                temp = temp[-5:]
+            ticksValores.append(temp)
 
         if 'Cotação' in td.text:
             select = True
         if 'Nro. Ações' in td.text:
             select = True
+        if 'Setor' in td.text:
+            select = True
+        if 'Vol' in td.text:
+            select = True
+        if 'mercado' in td.text:
+            select = True
+        if 'firma' in td.text:
+            select = True
+        if 'P/L' in td.text:
+            select = True
+        if 'P/VP' in td.text:
+            select = True
+        if 'P/EBIT' in td.text:
+            select = True
+        if 'PSR' in td.text:
+            select = True
+        if 'Yield' in td.text:
+            select = True
+        if 'EV / EBIT' in td.text:
+            select = True
+        if '(5a)' in td.text:
+            select = True
+        if 'LPA' in td.text:
+            select = True
+        if 'VPA' in td.text:
+            select = True
+        if 'Marg. Bruta' in td.text:
+            select = True
+        if 'Marg. EBIT' in td.text:
+            select = True
+        if 'Marg. Líquida' in td.text:
+            select = True
+        if 'ROIC' in td.text:
+            select = True
+        if 'ROE' in td.text:
+            select = True
+        if 'Dív. Bruta' in td.text:
+            select = True
+        if 'Disponibilidades' in td.text:
+            select = True
+        if 'Dív. Líquida' in td.text:
+            select = True
+        if 'Ativo Circulante' in td.text:
+            select = True
+        if 'Patrim. Líq' in td.text:
+            select = True
+        if 'Receita Líquida' in td.text:
+            select = True
+        if 'Lucro Líquido' in td.text:
+            select = True
 
         if 'EBIT' in td.text:
-            cont = cont + 1
+            contebit = contebit + 1
 
-    with open('valores.csv', 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(ticksValores)
+        if 'Ativo' in td.text:
+            contativo = contativo + 1
+
+    print(ticksValores)
+
+    try:
+        if (ticksValores[2] == 'Financeiros') and (ticksValores[0] != 'ITSA4'):
+            with open('valoresF.csv', 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(ticksValores)
+        else:
+            with open('valores.csv', 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(ticksValores)
+
+    except Exception as e:
+        print('pagina nao achada')
+        pass
+
+
+df1 = pd.read_csv('valores.csv')
+df2 = pd.read_csv('valoresF.csv')
+frames = [df1, df2]
+result = pd.concat(frames)
+result.to_csv('ValoresFinal.csv')
